251b7510e2b2d9d1d669eb4ef50954ec947bfdc4
Sercammus <sercammus@gmail.com>
2017-01-17 10:36:43 +0100

    Hoozza we are Xpanding!

---
+++ mde.lect7.generator/src-gen/Main.java
@@ -0,0 +1,44 @@
+
+public class Main {
+
+	public static void main() {
+
+		int state = 0;
+
+		while (state > 0) {
+			switch (state) {
+
+				case 0 :
+
+					chatServer(); //TODO parameters!
+
+					state = 0;
+
+					break;
+
+			}
+		}
+
+	}
+
+	public static void chatServer() {
+
+		int state = 0;
+
+		while (state > 0) {
+			switch (state) {
+
+				case 0 :
+
+					//Fork states here
+
+					state = 0;
+
+					break;
+
+			}
+		}
+
+	}
+
+}
